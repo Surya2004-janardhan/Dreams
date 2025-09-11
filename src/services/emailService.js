8d7f55916d3cc74e44079eb0f6786c688f3c8ba4
Surya Janardhan <chintalajanardhan2004@gmail.com>
2025-09-11 17:50:29 +0530

    fix : createTransport()

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -5,7 +5,7 @@ const logger = require("../config/logger");
  * Create email transporter
  */
 const createTransporter = () => {
-  return nodemailer.createTransporter({
+  return nodemailer.createTransport({
     service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
