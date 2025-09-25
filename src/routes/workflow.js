6670bd22d0afa365494ee2477b9ca605ccc2cdbe
Surya Janardhan <chintalajanardhan2004@gmail.com>
2025-09-25 23:45:56 +0530

    remove auto workflow dirs checking and creation

--- src/routes/workflow.js
+++ src/routes/workflow.js
@@ -9,23 +9,23 @@ const path = require("path");
 
 const router = express.Router();
 
-function ensureDirs(dirs) {
-  dirs.forEach((dir) => {
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir, { recursive: true });
-    }
-  });
-}
+// function ensureDirs(dirs) {
+//   dirs.forEach((dir) => {
+//     if (!fs.existsSync(dir)) {
+//       fs.mkdirSync(dir, { recursive: true });
+//     }
+//   });
+// }
 
 // POST /workflow/auto - Automated workflow (pulls from Google Sheets)
 router.post("/auto", (req, res, next) => {
-  ensureDirs([
-    path.join(__dirname, "../../audio"),
-    path.join(__dirname, "../../final_video"),
-    path.join(__dirname, "../../images"),
-    path.join(__dirname, "../../scripts"),
-    path.join(__dirname, "../../subtitles"),
-  ]);
+  // ensureDirs([
+  //   path.join(__dirname, "../../audio"),
+  //   path.join(__dirname, "../../final_video"),
+  //   path.join(__dirname, "../../images"),
+  //   path.join(__dirname, "../../scripts"),
+  //   path.join(__dirname, "../../subtitles"),
+  // ]);
   return runAutomatedWorkflow(req, res, next);
 });
 
