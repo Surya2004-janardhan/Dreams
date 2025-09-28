372a72ce8e93dccec231551ada54fd46d5281f95
Surya Janardhan <chintalajanardhan2004@gmail.com>
2025-09-28 10:08:27 +0530

    fix : email pwd

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -11,7 +11,7 @@ const createTransporter = () => {
     service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      pass: process.env.EMAIL_APP_PASSWORD,
     },
   });
 };
